1738e80cafa7c669e11ee46b1c4e4385ab5490dc
thesilentc <thesilentc@me.com>
2018-06-27 21:35:34 -0400

    update migration requirments

--- db/migrate/20180622021610_create_hosts.rb
+++ db/migrate/20180622021610_create_hosts.rb
@@ -1,8 +1,8 @@
 class CreateHosts < ActiveRecord::Migration
   def change
+    def change
     create_table :hosts do |t|
-
-      t.timestamps null: false
-    end
-  end
+      t.string :name
+     end
+   end
 end
