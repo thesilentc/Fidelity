cb42f8db6b8b447d24ff840849cf758210ecd996
thesilentc <thesilentc@me.com>
2018-07-24 21:09:35 -0400

    Hat class defined

--- app/models/hat.rb
+++ app/models/hat.rb
@@ -1,2 +1,16 @@
 class Hat < ActiveRecord::Base
+
+hatcolor  self
+
+def hatcolor
+  if hatcolor == 'black'
+    puts 'bad'
+
+  elseif hatcolor == 'white'
+  puts 'good'
+
+else puts 'unassigned'
+  end
+end
+
 end
