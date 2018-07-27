afa85acf7b6c8651626fa9cb2230d0b6d9d5b949
thesilentc <thesilentc@me.com>
2018-07-26 20:52:47 -0400

    notes

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+# set up the Active Record Connection to the app
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
