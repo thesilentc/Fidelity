e9246e19f0399e72fb3733147ca241af39995b7a
thesilentc <thesilentc@me.com>
2018-07-31 21:23:57 -0400

    testing migration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,20 +17,15 @@ class ApplicationController < Sinatra::Base
   end
 
   helpers do
-      def redirect_if_not_logged_in
-        if !logged_in?
-          redirect "/login?error=You have to be logged in to do that"
-        end
-      end
 
-      def logged_in?
-        !!session[:user_id]
-      end
-
-      def current_user
-        User.find(session[:user_id])
-      end
-
+    def logged_in?
+      !!current_user
     end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+  end
 
 end
